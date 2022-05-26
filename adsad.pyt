6667562472d7b42253a3a08735eb5b6b86f9c737
Adam Jastrzębski <ajastrzebski@paloaltonetworks.com>
2022-05-26 18:21:00 +0200

    msg

--- adsad.pyt
+++
@@ -1,2 +0,0 @@
-x = adsad
-y = "ble"
--- script.sh
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-  echo "TESTING"
-  if git diff --name-only HEAD^ HEAD | grep "CHANGELOG.md" | xargs
-  then
-    exit ">>>Please update CHANGELOG.md<<<"
-  fi
-=======
->>>>>>> fc76156a6204cbada557779bdc301d67bc05b9ef
